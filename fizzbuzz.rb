16a06aff00aaa9750501a5af95f06fd5b805ff54
Daniel Shimield <dxshim90@gmail.com>
2019-01-27 16:57:17 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(fizz)
+  if fizz % 5 == 0 && fizz % 3 == 0
+    return "FizzBuzz"
+  elsif fizz % 5 == 0
+  return "Buzz"
+  elsif fizz % 3 == 0
+  return "Fizz"
+end
+end
