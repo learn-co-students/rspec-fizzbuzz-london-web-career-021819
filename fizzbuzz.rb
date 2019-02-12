530abf0864c2bc0f5bb03708ef59088c4b0b20e5
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-12 13:57:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(numberIn)
+  return "FizzBuzz" if (numberIn % 3 == 0 && numberIn % 5 == 0)
+  return "Fizz" if (numberIn % 3 == 0)
+  return "Buzz" if (numberIn % 5 == 0)
+end
+ # Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do thi
